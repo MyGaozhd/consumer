b85b95d91f2cb7cb91f58971c45a99f028363b8c
高志东 <gaozhdf@yonyou.com>
2019-07-28 11:44:39 +0800

    增加eureka客户端注册

--- src/main/java/com/servi/cloud/consumer/ConsumerApplication.java
+++ src/main/java/com/servi/cloud/consumer/ConsumerApplication.java
@@ -2,6 +2,7 @@ package com.servi.cloud.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,15 +10,16 @@ import org.springframework.web.client.RestTemplate;
  * 服务提供者
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ConsumerApplication {
 
-	@Bean
-	public RestTemplate restTemplate(){
-		return  new RestTemplate();
-	}
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
 
 }
