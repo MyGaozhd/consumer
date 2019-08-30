5218b3127e40d112ed5394f0c700bb3046cb3ca6
高志东 <gaozhdf@yonyou.com>
2019-08-30 16:05:05 +0800

    增加线程池

--- src/main/java/com/servi/cloud/consumer/concurrent/threadpool/ThreadPoolTest.java
+++ src/main/java/com/servi/cloud/consumer/concurrent/threadpool/ThreadPoolTest.java
@@ -3,22 +3,27 @@ package com.servi.cloud.consumer.concurrent.threadpool;
 import com.servi.cloud.consumer.util.log.ServiLogger;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadPoolTest {
     private int count = 1;
     ExecutorService service = new ThreadPoolExecutor(count, count * 2,
             0L, TimeUnit.MILLISECONDS,
-            new ArrayBlockingQueue<>(2), Executors.defaultThreadFactory(), new RejectedExecutionHandler() {
+            new ArrayBlockingQueue<>(2), new DefaultThreadFactory(), new RejectedExecutionHandler() {
         @Override
         public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
             if (r instanceof TestRunnable) {
                 ServiLogger.log("拒绝当前任务：" + ((TestRunnable) r).getCount());
+                if (((TestRunnable) r).getCount() % 0 == 0) {
+
+                }
             }
         }
     });
 
     public void submit(Runnable r) {
-        service.submit(r);
+//        service.submit(r);
+        service.execute(r);
     }
 
     public static void main(String[] args) throws InterruptedException {
@@ -27,6 +32,8 @@ public class ThreadPoolTest {
 //            Thread.sleep(500);
             pool.submit(new TestRunnable(i));
         }
+
+        Thread.sleep(3000);
     }
 
     public static class TestRunnable implements Runnable {
@@ -43,6 +50,42 @@ public class ThreadPoolTest {
         @Override
         public void run() {
             ServiLogger.log("执行当前任务：" + count);
+//            if (count % 2 == 0) {
+                if (count % 0 == 0) {
+
+                }
+//            }
+            ServiLogger.log("执行当前任务：" + count);
+        }
+    }
+
+    /**
+     * The default thread factory
+     */
+    static class DefaultThreadFactory implements ThreadFactory {
+        private static final AtomicInteger poolNumber = new AtomicInteger(1);
+        private final ThreadGroup group;
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+        private final String namePrefix;
+
+        DefaultThreadFactory() {
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() :
+                    Thread.currentThread().getThreadGroup();
+            namePrefix = "pool-" +
+                    poolNumber.getAndIncrement() +
+                    "-thread-";
+        }
+
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r,
+                    namePrefix + threadNumber.getAndIncrement(),
+                    0);
+//            if (t.isDaemon())
+            t.setDaemon(true);
+            if (t.getPriority() != Thread.NORM_PRIORITY)
+                t.setPriority(Thread.NORM_PRIORITY);
+            return t;
         }
     }
 }
