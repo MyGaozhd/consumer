8521788f44273b4302c9b1fe01345e381f93a40b
高志东 <gaozhdf@yonyou.com>
2019-09-18 17:04:44 +0800

    增加elk调用链路跟踪

--- src/main/java/com/servi/cloud/consumer/controller/UserController.java
+++ src/main/java/com/servi/cloud/consumer/controller/UserController.java
@@ -3,6 +3,8 @@ package com.servi.cloud.consumer.controller;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.ribbon.proxy.annotation.Hystrix;
 import com.servi.cloud.consumer.entry.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,13 +14,14 @@ import org.springframework.web.client.RestTemplate;
 @RestController()
 public class UserController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("findUserById/{id}")
     @HystrixCommand(fallbackMethod = "fallbackMethod")
     public User findUserById(@PathVariable int id) {
-
+        log.error("findUserById:" + id);
         return restTemplate.getForObject("http://provider/user/" + id, User.class);
     }
 
--- src/main/java/com/servi/cloud/consumer/javabase/Test.java
+++ src/main/java/com/servi/cloud/consumer/javabase/Test.java
@@ -1,4 +1,11 @@
 package com.servi.cloud.consumer.javabase;
 
+import java.util.HashMap;
+
 public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
 }
