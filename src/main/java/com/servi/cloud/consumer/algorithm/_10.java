da56210547b6a1857bd97c6a114452fa6a63fff5
高志东 <gaozhdf@yonyou.com>
2020-01-10 17:00:28 +0800

    LeetCode: 单词拆分II

---
+++ src/main/java/com/servi/cloud/consumer/algorithm/_10.java
@@ -0,0 +1,149 @@
+package com.servi.cloud.consumer.algorithm;
+
+import java.util.*;
+
+/**
+ * 单词拆分
+ * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ * <p>
+ * 说明：
+ * <p>
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ * 示例 1：
+ * <p>
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ * 示例 2：
+ * <p>
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+ * 注意你可以重复使用字典中的单词。
+ * 示例 3：
+ * <p>
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ */
+public class _10 {
+
+    public static void main(String[] args) {
+
+//        System.out.println(ArrayUtils.toString("kkkkssss".split("kkkkssss")));
+//        String ks = "cars";
+//        List<String> wordDict = new ArrayList<>();
+//        wordDict.add("car");
+//        wordDict.add("ca");
+//        wordDict.add("rs");
+
+//        String ks = "leetcode";
+//        List<String> wordDict = new ArrayList<>();
+//        wordDict.add("leet");
+//        wordDict.add("code");
+
+        String ks = "ccaccc";
+        Set<String> wordDict = new HashSet<>();
+        wordDict.add("ac");
+        wordDict.add("cc");
+
+
+//        wordDict.add("cats");
+//        wordDict.add("dog");
+//        wordDict.add("sand");
+//        wordDict.add("and");
+//        wordDict.add("cat");
+
+        System.out.println(System.currentTimeMillis());
+
+//        String s = "fohhemkkaecojceoaejkkoedkofhmohkcjmkggcmnami";
+//        String[] ww = new String[]{"kfomka", "hecagbngambii", "anobmnikj", "c", "nnkmfelneemfgcl", "ah", "bgomgohl", "lcbjbg", "ebjfoiddndih", "hjknoamjbfhckb", "eioldlijmmla", "nbekmcnakif", "fgahmihodolmhbi", "gnjfe", "hk", "b", "jbfgm", "ecojceoaejkkoed", "cemodhmbcmgl", "j", "gdcnjj", "kolaijoicbc", "liibjjcini", "lmbenj", "eklingemgdjncaa", "m", "hkh", "fblb", "fk", "nnfkfanaga", "eldjml", "iejn", "gbmjfdooeeko", "jafogijka", "ngnfggojmhclkjd", "bfagnfclg", "imkeobcdidiifbm", "ogeo", "gicjog", "cjnibenelm", "ogoloc", "edciifkaff", "kbeeg", "nebn", "jdd", "aeojhclmdn", "dilbhl", "dkk", "bgmck", "ohgkefkadonafg", "labem", "fheoglj", "gkcanacfjfhogjc", "eglkcddd", "lelelihakeh", "hhjijfiodfi", "enehbibnhfjd", "gkm", "ggj", "ag", "hhhjogk", "lllicdhihn", "goakjjnk", "lhbn", "fhheedadamlnedh", "bin", "cl", "ggjljjjf", "fdcdaobhlhgj", "nijlf", "i", "gaemagobjfc", "dg", "g", "jhlelodgeekj", "hcimohlni", "fdoiohikhacgb", "k", "doiaigclm", "bdfaoncbhfkdbjd", "f", "jaikbciac", "cjgadmfoodmba", "molokllh", "gfkngeebnggo", "lahd", "n", "ehfngoc", "lejfcee", "kofhmoh", "cgda", "de", "kljnicikjeh", "edomdbibhif", "jehdkgmmofihdi", "hifcjkloebel", "gcghgbemjege", "kobhhefbbb", "aaikgaolhllhlm", "akg", "kmmikgkhnn", "dnamfhaf", "mjhj", "ifadcgmgjaa", "acnjehgkflgkd", "bjj", "maihjn", "ojakklhl", "ign", "jhd", "kndkhbebgh", "amljjfeahcdlfdg", "fnboolobch", "gcclgcoaojc", "kfokbbkllmcd", "fec", "dljma", "noa", "cfjie", "fohhemkka", "bfaldajf", "nbk", "kmbnjoalnhki", "ccieabbnlhbjmj", "nmacelialookal", "hdlefnbmgklo", "bfbblofk", "doohocnadd", "klmed", "e", "hkkcmbljlojkghm", "jjiadlgf", "ogadjhambjikce", "bglghjndlk", "gackokkbhj", "oofohdogb", "leiolllnjj", "edekdnibja", "gjhglilocif", "ccfnfjalchc", "gl", "ihee", "cfgccdmecem", "mdmcdgjelhgk", "laboglchdhbk", "ajmiim", "cebhalkngloae", "hgohednmkahdi", "ddiecjnkmgbbei", "ajaengmcdlbk", "kgg", "ndchkjdn", "heklaamafiomea", "ehg", "imelcifnhkae", "hcgadilb", "elndjcodnhcc", "nkjd", "gjnfkogkjeobo", "eolega", "lm", "jddfkfbbbhia", "cddmfeckheeo", "bfnmaalmjdb", "fbcg", "ko", "mojfj", "kk", "bbljjnnikdhg", "l", "calbc", "mkekn", "ejlhdk", "hkebdiebecf", "emhelbbda", "mlba", "ckjmih", "odfacclfl", "lgfjjbgookmnoe", "begnkogf", "gakojeblk", "bfflcmdko", "cfdclljcg", "ho", "fo", "acmi", "oemknmffgcio", "mlkhk", "kfhkndmdojhidg", "ckfcibmnikn", "dgoecamdliaeeoa", "ocealkbbec", "kbmmihb", "ncikad", "hi", "nccjbnldneijc", "hgiccigeehmdl", "dlfmjhmioa", "kmff", "gfhkd", "okiamg", "ekdbamm", "fc", "neg", "cfmo", "ccgahikbbl", "khhoc", "elbg", "cbghbacjbfm", "jkagbmfgemjfg", "ijceidhhajmja", "imibemhdg", "ja", "idkfd", "ndogdkjjkf", "fhic", "ooajkki", "fdnjhh", "ba", "jdlnidngkfffbmi", "jddjfnnjoidcnm", "kghljjikbacd", "idllbbn", "d", "mgkajbnjedeiee", "fbllleanknmoomb", "lom", "kofjmmjm", "mcdlbglonin", "gcnboanh", "fggii", "fdkbmic", "bbiln", "cdjcjhonjgiagkb", "kooenbeoongcle", "cecnlfbaanckdkj", "fejlmog", "fanekdneoaammb", "maojbcegdamn", "bcmanmjdeabdo", "amloj", "adgoej", "jh", "fhf", "cogdljlgek", "o", "joeiajlioggj", "oncal", "lbgg", "elainnbffk", "hbdi", "femcanllndoh", "ke", "hmib", "nagfahhljh", "ibifdlfeechcbal", "knec", "oegfcghlgalcnno", "abiefmjldmln", "mlfglgni", "jkofhjeb", "ifjbneblfldjel", "nahhcimkjhjgb", "cdgkbn", "nnklfbeecgedie", "gmllmjbodhgllc", "hogollongjo", "fmoinacebll", "fkngbganmh", "jgdblmhlmfij", "fkkdjknahamcfb", "aieakdokibj", "hddlcdiailhd", "iajhmg", "jenocgo", "embdib", "dghbmljjogka", "bahcggjgmlf", "fb", "jldkcfom", "mfi", "kdkke", "odhbl", "jin", "kcjmkggcmnami", "kofig", "bid", "ohnohi", "fcbojdgoaoa", "dj", "ifkbmbod", "dhdedohlghk", "nmkeakohicfdjf", "ahbifnnoaldgbj", "egldeibiinoac", "iehfhjjjmil", "bmeimi", "ombngooicknel", "lfdkngobmik", "ifjcjkfnmgjcnmi", "fmf", "aoeaa", "an", "ffgddcjblehhggo", "hijfdcchdilcl", "hacbaamkhblnkk", "najefebghcbkjfl", "hcnnlogjfmmjcma", "njgcogemlnohl", "ihejh", "ej", "ofn", "ggcklj", "omah", "hg", "obk", "giig", "cklna", "lihaiollfnem", "ionlnlhjckf", "cfdlijnmgjoebl", "dloehimen", "acggkacahfhkdne", "iecd", "gn", "odgbnalk", "ahfhcd", "dghlag", "bchfe", "dldblmnbifnmlo", "cffhbijal", "dbddifnojfibha", "mhh", "cjjol", "fed", "bhcnf", "ciiibbedklnnk", "ikniooicmm", "ejf", "ammeennkcdgbjco", "jmhmd", "cek", "bjbhcmda", "kfjmhbf", "chjmmnea", "ifccifn", "naedmco", "iohchafbega", "kjejfhbco", "anlhhhhg"};
+//        System.out.println(new _10().wordBreak(s, Arrays.asList(ww)));
+
+        System.out.println(new _10().wordBreak(ks, wordDict).toString());
+//        System.out.println("cccc".split("cc").length);
+        System.out.println(System.currentTimeMillis());
+
+//        Set<String> wordDict = new HashSet<>();
+        Set<String> swordDict = new HashSet<>();
+        for (int i = 0; i <wordDict.size() ; i++) {
+            swordDict.addAll(wordDict);
+        }
+
+    }
+
+    public List<String> wordBreak(String s, Set<String> wordDict) {
+        return word_Break(s, wordDict, 0);
+    }
+    HashMap<Integer, List<String>> map = new HashMap<>();
+
+    public List<String> word_Break(String s, Set<String> wordDict, int start) {
+        if (map.containsKey(start)) {
+            return map.get(start);
+        }
+        LinkedList<String> res = new LinkedList<>();
+        if (start == s.length()) {
+            res.add("");
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (wordDict.contains(s.substring(start, end))) {
+                List<String> list = word_Break(s, wordDict, end);
+                for (String l : list) {
+                    res.add(s.substring(start, end) + (l.equals("") ? "" : " ") + l);
+                }
+            }
+        }
+        map.put(start, res);
+        return res;
+    }
+
+//    public List<String> wordBreak(String s, List<String> wordDict) {
+//        Set<String> wordAlready = new HashSet<>();
+//        Map<String, List<String>> already = new HashMap<>();
+//
+//        List<String> wordBreak = wordBreak1(s, wordDict, already);
+//        return wordBreak;
+//    }
+
+    public List<String> wordBreak1(String s, List<String> wordDict, Map<String, List<String>> allready) {
+
+        List<String> subWordBreak = new ArrayList<>();
+
+        for (int i = 0; i < wordDict.size(); i++) {
+            String word = wordDict.get(i);
+            String[] a = s.split(word);
+
+            if (a.length == 1 && a[0].equals(s)) {
+                continue;
+            }
+
+            if (a.length == 0) {
+                int b = s.length() / word.length();
+                String sub = "";
+                for (int j = 0; j < b; j++) {
+                    sub = sub + " " + word;
+                }
+                subWordBreak.add(word);
+                continue;
+            } else {
+                List<String> subResult = new ArrayList<>();
+                for (int j = 0; j < a.length; j++) {
+                    if (a[j].equals("")) {
+                        continue;
+                    }
+                    if (allready.containsKey(a[j])) {
+                        subResult = allready.get(a[j]);
+                    } else {
+                        subResult = wordBreak1(a[j], wordDict, allready);
+                        allready.put(a[j], subResult);
+                    }
+                }
+                for (int j = 0; j < subResult.size(); j++) {
+                    subWordBreak.add(word + " " + subResult.get(i));
+                }
+            }
+        }
+
+        return subWordBreak;
+    }
+}
