285ccdbefdfa1e81c6304ff649efab7fd7838782
高志东 <gaozhdf@yonyou.com>
2020-04-03 13:34:16 +0800

    自定义类加载器测试

---
+++ src/main/java/com/servi/cloud/consumer/javabase/fanxing/T.java
@@ -0,0 +1,27 @@
+package com.servi.cloud.consumer.javabase.fanxing;
+
+import java.util.concurrent.Callable;
+
+public class T {
+
+    public static void main(String[] args) {
+        T.run(new TT());
+    }
+
+    public static  <E extends  Runnable & Callable> void run (E e){
+
+    }
+
+    static class  TT implements Runnable,Callable{
+
+        @Override
+        public void run() {
+
+        }
+
+        @Override
+        public Object call() throws Exception {
+            return null;
+        }
+    }
+}
---
+++ src/main/java/com/servi/cloud/consumer/javabase/jvm/ClassLoaderTest07.java
@@ -0,0 +1,15 @@
+package com.servi.cloud.consumer.javabase.jvm;
+
+import java.util.concurrent.Callable;
+
+public class ClassLoaderTest07 {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("sun.boot.class.path"));
+        System.out.println("\r\n");
+        System.out.println(System.getProperty("java.ext.dirs"));
+        System.out.println("\r\n");
+        System.out.println(System.getProperty("java.class.path"));
+    }
+
+
+}
