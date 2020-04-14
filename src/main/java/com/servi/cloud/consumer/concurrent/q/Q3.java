3e6ab1e7dbfcbf7e33a2734a8ed099c2235f9d7b
高志东 <gaozhdf@yonyou.com>
2020-04-14 21:39:51 +0800

    提交多线程环境下的并发问题

--- src/main/java/com/servi/cloud/consumer/concurrent/q/Q3.java
+++ src/main/java/com/servi/cloud/consumer/concurrent/q/Q3.java
@@ -1,9 +1,30 @@
 package com.servi.cloud.consumer.concurrent.q;
 
+/**
+ * volatile 保证可见性和有序性 解释原子性问题
+ */
 public class Q3 {
-    int count = 0;
+    private static volatile long count = 0;
 
-    public void add() {
-        count += 1;
+    private void add() {
+        int i = 0;
+        while (i++ < 10000) {
+            count += 1;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final Q3 q = new Q3();
+        // 创建两个线程，执行add()操作
+        Thread t1 = new Thread(q::add);
+        Thread t2 = new Thread(q::add);
+
+        // 启动两个线程
+        t1.start();
+        t2.start();
+        // 等待两个线程执行结束
+        t1.join();
+        t2.join();
+        System.out.println(count);
     }
 }
